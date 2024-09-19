d4bdb84a00f73a0121229c07887d3ef330542020
German Rojas <g.rojasc@uniandes.edu.co>
2024-09-18 19:15:04 -0500

    Refactor Detail component to fetch and display specific pet details

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,9 +1,29 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 export default function Detail() {
- const params = useParams();
+  const { mascotaId } = useParams();
+  const [mascota, setMascota] = useState(null);
+
+  useEffect(() => {
+    const URL = `https://gist.githubusercontent.com/josejbocanegra/829a853c6c68880477697acd0490cecc/raw/99c31372b4d419a855e53f0e891246f313a71b20/mascotas.json`;
+    fetch(URL)
+      .then((data) => data.json())
+      .then((data) => {
+        const mascota = data.find((m) => m.id === parseInt(mascotaId));
+        setMascota(mascota);
+      });
+  }, [mascotaId]);
+
+  if (!mascota) {
+    return <h1>Loading...</h1>;
+  }
+
  return (
-   <div>
-     <h1>I am {params.mascotaId}</h1>
+   <div className="container mt-4">
+      <h1>{mascota.nombre}</h1>
+      <img src={mascota.foto} alt={mascota.descripcion} />
+      <p>{mascota.descripcion}</p>
+
    </div>
  );
 }
